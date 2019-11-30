c493c41ff4b74e4c5baca5cde398994a2596887c
Alex Sullivan <Alexsullivan114@gmail.com>
2019-11-30 13:54:13 -0500

    Adjust styles

--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
@@ -20,51 +20,51 @@ import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MaterialTheme {
-                Surface(color = Color(250, 250, 250)) {
-                    ListOfStuff()
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MaterialTheme {
+        Surface(color = Color(250, 250, 250)) {
+          ListOfStuff()
         }
+      }
     }
+  }
 }
 
 @Composable
 @Preview
 fun ListOfStuff() {
-    VerticalScroller() {
-        Column(crossAxisSize = LayoutSize.Expand) {
-            IngredientCard(R.drawable.noodles)
-            IngredientCard(R.drawable.pizza)
-            IngredientCard(R.drawable.produce)
-            IngredientCard(R.drawable.salad_egg)
-            IngredientCard(R.drawable.smoothie)
-            IngredientCard(R.drawable.croissant)
-        }
+  VerticalScroller() {
+    Column(crossAxisSize = LayoutSize.Expand) {
+      IngredientCard(R.drawable.noodles)
+      IngredientCard(R.drawable.pizza)
+      IngredientCard(R.drawable.produce)
+      IngredientCard(R.drawable.salad_egg)
+      IngredientCard(R.drawable.smoothie)
+      IngredientCard(R.drawable.croissant)
     }
+  }
 }
 
 fun IngredientCard(@DrawableRes imageRes: Int) {
-    val image = +imageResource(imageRes)
-    Padding(16.dp) {
-        Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
-            Column(crossAxisSize = LayoutSize.Expand) {
-                Container(expanded = true, height = 144.dp) {
-                    DrawImage(image = image)
-                }
-                Column(modifier = Spacing(16.dp)) {
-                    Text("Ingredients", style = +themeTextStyle { h4 })
-                    Text("• Red Kidney Beans")
-                    Text("• Onions")
-                    Text("• Ginger")
-                    Text("• Garlic")
-                    Text("• Time!")
-                }
-            }
+  val image = +imageResource(imageRes)
+  Padding(16.dp) {
+    Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
+      Column(crossAxisSize = LayoutSize.Expand) {
+        Container(expanded = true, height = 144.dp) {
+          DrawImage(image = image)
         }
+        Column(modifier = Spacing(16.dp)) {
+          Text("Ingredients", style = +themeTextStyle { h4 })
+          Text("• Red Kidney Beans")
+          Text("• Onions")
+          Text("• Ginger")
+          Text("• Garlic")
+          Text("• Time!")
+        }
+      }
     }
+  }
 }
 
