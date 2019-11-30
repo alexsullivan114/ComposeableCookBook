d10f7e3d24154a341b1f6c5e5476b2bd6ac63b01
Alex Sullivan <Alexsullivan114@gmail.com>
2019-11-30 14:07:34 -0500

    Dynamic recipes

--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.raywenderlich.android.ComposableCookBook
 
 import android.os.Bundle
-import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.compose.unaryPlus
@@ -17,7 +16,6 @@ import androidx.ui.material.MaterialTheme
 import androidx.ui.material.surface.Surface
 import androidx.ui.material.themeTextStyle
 import androidx.ui.res.imageResource
-import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     setContent {
       MaterialTheme {
         Surface(color = Color(250, 250, 250)) {
-          ListOfStuff()
+          RecipeList(defaultRecipes)
         }
       }
     }
@@ -33,22 +31,19 @@ class MainActivity : AppCompatActivity() {
 }
 
 @Composable
-@Preview
-fun ListOfStuff() {
-  VerticalScroller() {
+fun RecipeList(recipes: List<Recipe>) {
+  VerticalScroller {
     Column(crossAxisSize = LayoutSize.Expand) {
-      IngredientCard(R.drawable.noodles)
-      IngredientCard(R.drawable.pizza)
-      IngredientCard(R.drawable.produce)
-      IngredientCard(R.drawable.salad_egg)
-      IngredientCard(R.drawable.smoothie)
-      IngredientCard(R.drawable.croissant)
+      for (recipe in recipes) {
+        RecipeCard(recipe)
+      }
     }
   }
 }
 
-fun IngredientCard(@DrawableRes imageRes: Int) {
-  val image = +imageResource(imageRes)
+@Composable
+fun RecipeCard(recipe: Recipe) {
+  val image = +imageResource(recipe.imageResource)
   Padding(16.dp) {
     Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
       Column(crossAxisSize = LayoutSize.Expand) {
@@ -56,12 +51,10 @@ fun IngredientCard(@DrawableRes imageRes: Int) {
           DrawImage(image = image)
         }
         Column(modifier = Spacing(16.dp)) {
-          Text("Ingredients", style = +themeTextStyle { h4 })
-          Text("• Red Kidney Beans")
-          Text("• Onions")
-          Text("• Ginger")
-          Text("• Garlic")
-          Text("• Time!")
+          Text(recipe.title, style = +themeTextStyle { h4 })
+          recipe.ingredients.forEach {
+            Text("• $it")
+          }
         }
       }
     }
---
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/Recipe.kt
@@ -0,0 +1,5 @@
+package com.raywenderlich.android.ComposableCookBook
+
+import androidx.annotation.DrawableRes
+
+data class Recipe(@DrawableRes val imageResource: Int, val title: String, val ingredients: List<String>)
