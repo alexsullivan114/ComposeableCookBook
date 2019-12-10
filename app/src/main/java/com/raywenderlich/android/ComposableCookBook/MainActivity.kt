bdcf943f7b1a77d654f759be688d0a558d6ba028
Alex Sullivan <Alexsullivan114@gmail.com>
2019-12-09 20:06:55 -0500

    Remove non starter files and add a layout

--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
@@ -2,14 +2,11 @@ package com.raywenderlich.android.ComposableCookBook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.ui.core.setContent
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      ComposableCookBookApp()
-    }
+    setContentView(R.layout.activity_main)
   }
 }
 
--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/Recipe.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/Recipe.kt
@@ -1,9 +1,7 @@
 package com.raywenderlich.android.ComposableCookBook
 
 import androidx.annotation.DrawableRes
-import androidx.compose.Model
 
-@Model
 data class Recipe(@DrawableRes val imageResource: Int,
                   val title: String,
                   val ingredients: List<String>,
