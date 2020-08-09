f89bda1f15d87648101fb562000a075378796da2
Alex Sullivan <alexsullivan114@gmail.com>
2020-08-09 15:04:24 -0400

    wip

--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
@@ -3,11 +3,12 @@ package com.raywenderlich.android.ComposableCookBook
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.Text
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Text
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
-import androidx.ui.layout.FlexColumn
+import androidx.ui.layout.fillMaxSize
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.TopAppBar
 import androidx.ui.text.TextStyle
@@ -18,15 +19,11 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContent {
       MaterialTheme {
-        FlexColumn {
-          inflexible {
-            TopAppBar(title = {
-              Text("ComposableCookBook")
-            })
-          }
-          flexible(flex = 1f) {
-            RecipeList(defaultRecipes)
-          }
+        Column(modifier = Modifier.fillMaxSize()) {
+          TopAppBar(title = {
+            Text("ComposableCookBook")
+          })
+          RecipeList(defaultRecipes)
         }
       }
     }
--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/RecipeCard.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/RecipeCard.kt
@@ -1,30 +1,32 @@
 package com.raywenderlich.android.ComposableCookBook
 
 import androidx.compose.Composable
-import androidx.compose.unaryPlus
-import androidx.ui.core.Text
-import androidx.ui.core.dp
-import androidx.ui.foundation.DrawImage
+import androidx.ui.core.Modifier
+import androidx.ui.foundation.Box
+import androidx.ui.foundation.Image
+import androidx.ui.foundation.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.Column
-import androidx.ui.layout.Container
-import androidx.ui.layout.Spacing
+import androidx.ui.layout.fillMaxSize
+import androidx.ui.layout.fillMaxWidth
+import androidx.ui.layout.size
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.surface.Surface
-import androidx.ui.material.themeTextStyle
+import androidx.ui.material.Surface
 import androidx.ui.res.imageResource
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 @Composable
 fun RecipeCard(recipe: Recipe) {
   Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
-    val image = +imageResource(recipe.imageResource)
-    Column {
-      Container(expanded = true, height = 144.dp) {
-        DrawImage(image = image)
+    val image = imageResource(recipe.imageResource)
+    Column(modifier = Modifier.fillMaxSize()) {
+      Box(modifier = Modifier.fillMaxWidth()) {
+        Image(asset = image)
       }
-      Column(modifier = Spacing(16.dp)) {
-        Text(recipe.title, style = +themeTextStyle { h4 })
+      // TODO: There used to be 16dp of spacing here.
+      Column() {
+        Text(recipe.title, style = MaterialTheme.typography.h4)
         for (ingredient in recipe.ingredients) {
           Text("• $ingredient")
         }
