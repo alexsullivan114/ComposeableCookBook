9a3629d5c0af7f57faa306f332934923d3a5e10d
Alex Sullivan <alexsullivan114@gmail.com>
2020-09-06 09:48:22 -0400

    Update starter project

--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/MainActivity.kt
@@ -2,41 +2,11 @@ package com.raywenderlich.android.ComposableCookBook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.TextStyle
-import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContent {
-      MaterialTheme {
-        Column(modifier = Modifier.fillMaxSize()) {
-          TopAppBar(title = {
-            Text("ComposableCookBook")
-          })
-          RecipeList(defaultRecipes)
-        }
-      }
-    }
-  }
-}
-
-@Composable
-@Preview
-fun Greeting() {
-  Column {
-    Text("Hello, World!", style = TextStyle(color = Color.Red))
-    Text("Hello, Second World!", style = TextStyle(color = Color.Red))
-    Text("Hello, Third World!", style = TextStyle(color = Color.Red))
+    setContentView(R.layout.activity_main)
   }
 }
 
