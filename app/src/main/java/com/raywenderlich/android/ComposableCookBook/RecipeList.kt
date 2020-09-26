1dd075ff9f7b253765896bd361e63214b0d005f9
Alex Sullivan <alexsullivan114@gmail.com>
2020-09-26 14:09:09 -0400

    Use lazy column for instead of scrollable column

--- app/src/main/java/com/raywenderlich/android/ComposableCookBook/RecipeList.kt
+++ app/src/main/java/com/raywenderlich/android/ComposableCookBook/RecipeList.kt
@@ -3,6 +3,8 @@ package com.raywenderlich.android.composableCookBook
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyColumnItems
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -10,12 +12,8 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecipeList(recipes: List<Recipe>) {
-  ScrollableColumn {
-    Column {
-      for (recipe in recipes) {
-          RecipeCard(recipe, Modifier.padding(16.dp))
-      }
-    }
+  LazyColumnFor(items = recipes) { item ->
+    RecipeCard(item, Modifier.padding(16.dp))
   }
 }
 
